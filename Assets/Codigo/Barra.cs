e2fb65a6d6dde9beb7fe1e69382b7a218a2624c8
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2016-09-18 22:21:38 -0600

    Estructura basica y movimiento de barra en script

---
+++ Assets/Codigo/Barra.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barra : MonoBehaviour {
+	
+	public float velocidad = 5;
+	Vector3 posicionInicial;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start");
+		posicionInicial = transform.position;
+	}
+
+	public void Reset(){
+		transform.position = posicionInicial;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float tecaldoX = Input.GetAxisRaw ("Horizontal");
+		float posX = transform.position.x + (tecaldoX * velocidad * Time.deltaTime);
+		transform.position = new Vector3(Mathf.Clamp(posX,-8,8),transform.position.y,transform.position.z);
+
+		
+		Debug.Log ("x= "+posX);
+	}
+}
